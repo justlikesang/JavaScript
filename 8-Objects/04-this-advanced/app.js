1722318c3b04b2832ca7a8ac110b6b2c401083fa
Sang <sanghosuh@gmail.com>
2022-03-28 01:14:19 -0700

    advaned this review

--- 8-Objects/04-this-advanced/app.js
+++ 8-Objects/04-this-advanced/app.js
@@ -5,8 +5,6 @@ defaults to global - window
 arrow functions - pump the breaks
 */
 
-// console.log(this);
-
 function showThis() {
   console.log(this);
 }
@@ -25,11 +23,49 @@ john.showThis();
 bob.showThis();
 
 showThis();
+
 const btn1 = document.querySelector('.btn-1');
 const btn2 = document.querySelector('.btn-2');
 
 btn1.addEventListener('click', showThis);
 btn2.addEventListener('click', showThis);
 btn2.addEventListener('click', function() {
-  showThis();
+  showThis()
 });
+
+
+/* In Reg Functions (not arrow) 'this'
+determined by "HOW" !!! a function is invoked (left of .)
+
+defaults to global - window
+arrow functions - pump the breaks
+*/
+
+// console.log(this);
+
+// function showThis() {
+//   console.log(this);
+// }
+
+// const john = {
+//   name: 'john',
+//   showThis: showThis,
+// };
+
+// const bob = {
+//   name: 'bob',
+//   showThis: showThis,
+// };
+
+// john.showThis();
+// bob.showThis();
+
+// showThis();
+// const btn1 = document.querySelector('.btn-1');
+// const btn2 = document.querySelector('.btn-2');
+
+// btn1.addEventListener('click', showThis);
+// btn2.addEventListener('click', showThis);
+// btn2.addEventListener('click', function() {
+//   showThis();
+// });

eb9071f4c24d54b7809b5e4450950b28c5791a50
Sang <sanghosuh@gmail.com>
2022-03-31 23:02:56 -0700

    constructor function review

--- 8-Objects/06-constructor-functions/app.js
+++ 8-Objects/06-constructor-functions/app.js
@@ -3,8 +3,6 @@
 // Constructor Functions
 // new - creates new object, points to it, omit return
 
-// Constructor Function
-// capitalized function name signal's a special case
 function Person(firstName, lastName) {
   this.firstName = firstName;
   this.lastName = lastName;
@@ -13,29 +11,58 @@ function Person(firstName, lastName) {
       `My full name is ${this.firstName} ${this.lastName} and I love React`
     );
   };
-  // console log of 'this' will point to window object without
-  // the 'new' keyword
   console.log(this);
 }
 
 const john = new Person('john', 'anderson');
-console.log(john);
 john.fullName();
-
-const bob = Person('bob', 'jordan');
-/* console log without 'new' will return undefined,
- since we took out the new which omits return */
-console.log(bob);
+const bob = new Person('bob', 'jordan');
 bob.fullName();
 
 function createPerson(firstName, lastName) {
   return {
     firstName: firstName,
     lastName: lastName,
-    fullName: function () {
-      console.log(
-        `My full name is ${this.firstName} ${this.lastName} and I love JS`
-      );
+    // shorthand of creating method
+    fullName() {
+      console.log(`My full name is ${this.firstName} ${this.lastName}`);
     },
   };
 }
+
+// Constructor Function
+// capitalized function name signal's a special case
+// function Person(firstName, lastName) {
+//   this.firstName = firstName;
+//   this.lastName = lastName;
+//   this.fullName = function () {
+//     console.log(
+//       `My full name is ${this.firstName} ${this.lastName} and I love React`
+//     );
+//   };
+//   // console log of 'this' will point to window object without
+//   // the 'new' keyword
+//   console.log(this);
+// }
+
+// const john = new Person('john', 'anderson');
+// console.log(john);
+// john.fullName();
+
+// const bob = Person('bob', 'jordan');
+// /* console log without 'new' will return undefined,
+//  since we took out the new which omits return */
+// console.log(bob);
+// bob.fullName();
+
+// function createPerson(firstName, lastName) {
+//   return {
+//     firstName: firstName,
+//     lastName: lastName,
+//     fullName: function () {
+//       console.log(
+//         `My full name is ${this.firstName} ${this.lastName} and I love JS`
+//       );
+//     },
+//   };
+// }

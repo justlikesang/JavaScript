9456f79e1fdfea4b9428ee9f11731c67bd74bb32
Sang <sanghosuh@gmail.com>
2022-04-06 23:07:05 -0700

    prototype review

--- 8-Objects/08-prototypal-inheritance/app.js
+++ 8-Objects/08-prototypal-inheritance/app.js
@@ -18,14 +18,16 @@ function Account(name, initialBalance) {
   // };
 }
 
-const john = new Account('john', 200);
-const bob = new Account('bob', 0);
-
 Account.prototype.bank = 'Chase';
 Account.prototype.deposit = function (amount) {
   this.balance += amount;
   console.log(`Hello ${this.name}, your balance is ${this.balance}`);
 };
+
+const john = new Account('john', 200);
+const bob = new Account('bob', 0);
+
+console.log(john);
 
 john.deposit(300);
 
@@ -50,7 +52,6 @@ const testOne = new factoryTest('sang', 'suh');
 factoryTest.prototype.doesItWork = function (string) {
   console.log(`hello ${string}`);
 }
-
 
 testOne.doesItWork('world');
 
--- 8-Objects/09-lookup/app.js
+++ 8-Objects/09-lookup/app.js
@@ -7,29 +7,55 @@ Everything in JS is an Object
 function Account(name, initialBalance) {
   this.name = name;
   this.balance = initialBalance;
-  this.bank = 'Bank Of America'
+  this.bank = 'Bank of America';
 }
-const john = new Account('john', 200);
-const bob = new Account('bob', 0);
 
-// using deposit method without assigning a prototype
-/* when we have 1000+ customers for a bank, we'll be creating
- that many functions for the 'this.deposit' which is a waste of space */
-
-// john.deposit(1000);
-
-// console.log(Account.prototype);
-
-// using prototype to assign the function which can be shared across all the instances
-/* we now save memory by having it at 'one' place in a sense rather than for each instances */
-
-Account.prototype.bank = 'CHASE';
+Account.prototype.bank = 'Chase';
 Account.prototype.deposit = function (amount) {
   this.balance += amount;
   console.log(`Hello ${this.name}, your balance is ${this.balance}`);
 };
 
+const john = new Account('john', 200);
+const bob = new Account('bob', 0);
+
 // console.log(john.bank);
 console.log(bob);
 console.log({});
 console.log([]);
+
+console.log(john.constructor);
+console.log(Account.constructor);
+
+// john.deposit(300);
+// bob.deposit(1000);
+
+// function Account(name, initialBalance) {
+//   this.name = name;
+//   this.balance = initialBalance;
+//   this.bank = 'Bank Of America'
+// }
+// const john = new Account('john', 200);
+// const bob = new Account('bob', 0);
+
+// // using deposit method without assigning a prototype
+// /* when we have 1000+ customers for a bank, we'll be creating
+//  that many functions for the 'this.deposit' which is a waste of space */
+
+// // john.deposit(1000);
+
+// // console.log(Account.prototype);
+
+// // using prototype to assign the function which can be shared across all the instances
+// /* we now save memory by having it at 'one' place in a sense rather than for each instances */
+
+// Account.prototype.bank = 'CHASE';
+// Account.prototype.deposit = function (amount) {
+//   this.balance += amount;
+//   console.log(`Hello ${this.name}, your balance is ${this.balance}`);
+// };
+
+// // console.log(john.bank);
+// console.log(bob);
+// console.log({});
+// console.log([]);
